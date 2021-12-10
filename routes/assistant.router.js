ebd63e444d507dd01dbb8d59a870aa8457e6db20
Benjamín von Potobsky <bvonpotobsky@gmail.com>
2021-12-10 00:49:25 -0300

    Validator for request created and implemented
    boom library installed

--- routes/assistant.router.js
+++ routes/assistant.router.js
@@ -5,15 +5,22 @@ const router = express.Router();
 const AssistantService = require("../services/assistant.service");
 const service = new AssistantService();
 
-router.post("/", async (req, res) => {
-  try {
-    const { body } = req;
-    const newAssistant = await service.createAssistant(body);
-    res.status(201).json(newAssistant);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
+const validatorHandler = require("../middlewares/validator.handler");
+const { createAssistantSchema } = require("../schemas/assistant.schema");
+
+router.post(
+  "/",
+  validatorHandler(createAssistantSchema, "body"),
+  async (req, res) => {
+    try {
+      const { body } = req;
+      const newAssistant = await service.createAssistant(body);
+      res.status(201).json(newAssistant);
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
   }
-});
+);
 
 router.get("/", async (req, res) => {
   try {
